b387cb408b2d865240dd2a3f4c2652fe8874a102
Dmitry Vasiliev <dima@hlabs.spb.ru>
2009-09-13 14:27:46 +0400

    Added simple test client

---
+++ test_client.erl
@@ -0,0 +1,27 @@
+-module(test_client).
+
+-export([http_request/1]).
+
+-export([handle_headers/3, handle_body/2]).
+
+-behaviour(http_client).
+
+
+http_request(Url) ->
+    http_client:http_request(Url, [], ?MODULE, []).
+
+handle_headers({_, StatNum, _}=Status, Headers, _State)
+        when StatNum =:= 301; StatNum =:= 302 ->
+    % TODO: Limit number of redirects
+    io:format("Redirect. Status: ~p~nHeaders: ~p~n~n", [Status, Headers]),
+    Location = proplists:get_value('Location', Headers),
+    {redirect, http_request(binary_to_list(Location))};
+handle_headers(Status, Headers, State) ->
+    io:format("Status: ~p~nHeaders: ~p~n~n", [Status, Headers]),
+    {ok, State}.
+
+handle_body(eof, _State) ->
+    io:format("End.~n");
+handle_body(Chunk, State) ->
+    io:format("Chunk:~n~p~n~n", [Chunk]),
+    {ok, State}.
