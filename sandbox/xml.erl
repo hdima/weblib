2ebe7416f56d01575cce139621debd9cc5a42bd0
Dmitry Vasiliev <dima@hlabs.spb.ru>
2009-09-15 18:35:32 +0400

    Added new feed() interface

--- sandbox/xml.erl
+++ sandbox/xml.erl
@@ -1,19 +1,44 @@
 -module(xml).
 
--export([test/1]).
+-export([feed/2, feed/1]).
 
 
-test(Xml) ->
-    xmerl_sax_parser:stream(Xml, [
+feed(Xml) ->
+    Client = self(),
+    spawn_monitor(fun () -> parse(Xml, Client) end).
+
+
+feed(eof, {Server, Monitor}) ->
+    Server ! {self(), <<>>},
+    receive
+        {Server, Result} ->
+            Result;
+        {'DOWN', Monitor, process, Server, Reason} ->
+            {error, Reason}
+    after
+        100 ->
+            timeout
+    end;
+feed(Xml, {Server, _}) ->
+    Server ! {self(), Xml}.
+
+
+parse(Xml, Client) ->
+    Result = xmerl_sax_parser:stream(Xml, [
         {continuation_fun, fun next/1},
+        {continuation_state, Client},
         {event_fun, fun handle_event/3}
-    ]).
+    ]),
+    Client ! {self(), Result}.
 
 
-handle_event(Event, _Location, _State) ->
+handle_event(Event, _Location, State) ->
     io:format("~p~n", [Event]),
-    ok.
+    State.
 
 
-next(_State) ->
-    {<<>>, ok}.
+next(Client) ->
+    receive
+        {Client, Xml} ->
+            {Xml, Client}
+    end.
